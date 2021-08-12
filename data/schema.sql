5e1b87f958559bf8d5869586bf91c67fa683572b
Frank G <fgauss3@gmail.com>
2021-08-12 12:20:43 -0400

    schema updated

--- data/schema.sql
+++ data/schema.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE qaapi;
 
 \connect qaapi;
@@ -6,18 +7,18 @@ CREATE TABLE IF NOT EXISTS questions (
   id SERIAL PRIMARY KEY NOT NULL,
   product_id INTEGER NOT NULL,
   body TEXT NOT NULL,
-  date_written TEXT NOT NULL,
+  date_written BIGINT NOT NULL,
   asker_name TEXT NOT NULL,
   asker_email TEXT NOT NULL,
   reported INTEGER NOT NULL,
-  helpful INTEGER NOT NULL,
+  helpful INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS answers (
   id SERIAL NOT NULL PRIMARY KEY,
   question_id INTEGER NOT NULL,
   body TEXT NOT NULL,
-  date_written TIMESTAMP DEFAULT CURRENT_TIMESTAMP, NOT NULL
+  date_written BIGINT NOT NULL,
   answerer_name TEXT NOT NULL,
   answerer_email TEXT NOT NULL,
   reported INTEGER NOT NULL,
@@ -30,3 +31,30 @@ CREATE TABLE IF NOT EXISTS photos (
   url VARCHAR(2083) NOT NULL
 );
 
+/***************Data Load********************/
+
+
+COPY answers FROM '/Users/franklyspeaking/Desktop/answers.csv' WITH DELIMITER ',' CSV HEADER;
+
+COPY questions FROM '/Users/franklyspeaking/Desktop/questions.csv' WITH DELIMITER ',' CSV HEADER;
+
+COPY photos FROM '/Users/franklyspeaking/Desktop/answers_photos.csv' WITH DELIMITER ',' CSV HEADER;
+
+/********works********/
+alter table answers
+alter date_written type varchar;
+
+alter table answers
+alter date_written type bigint using date_written::bigint;
+
+alter table answers
+alter date_written type varchar using date_written::to_char(varchar2, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"');
+
+/*******converts unix-timestamp into js equivalent datetime*******/
+
+select to_timestamp(date_written/1000) from answers limit 1;
+
+select to_char(to_timestamp(date_written/1000), 'YYYY-MM-DD"T"HH24:MI:SS.MSZ') from answers limit 1;
+
+alter table answers
+alter date_written type varchar using to_char(to_timestamp(date_written/1000), 'YYYY-MM-DD"T"HH24:MI:SS.MSZ');
